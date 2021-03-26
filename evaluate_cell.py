c8d4f1e4ee23edefc340454bafbdb9568bfe1263
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2021-03-26 18:38:21 +0100

    Create evaluate_cell.py

---
+++ evaluate_cell.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def calculate_2d_number_of_facades(cells):
+    """
+    Returns the amount of free facades of the floorplan.
+    """
+    pass
+
+
+def calculate_2d_compactness(cells):
+    """
+    Returns the compactness of a floorplan as the ratio between area and perimeter.
+    """
+    pass
+
+
+def calculate_2d_shortest_distance_from_perimeter(cells):
+    pass
+
+
+def calculate_2d_patios(cells):
+    pass
+
+
+def calculate_3d_stability(cells):
+    pass
+
+
+def calculate_3d_daylight(cells):
+    pass

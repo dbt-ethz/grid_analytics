c3cc097a3869369c2c9cc314afaf218016e0a8b3
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2021-03-29 11:39:23 +0200

    Update analytics_3d.py

--- analytics_3d.py
+++ analytics_3d.py
@@ -63,8 +63,8 @@ def analyze_3d_grid(model3d, analysis_type="facade", ray1 = (1,1,0) ):
         array3d = shadow_map
 
     elif analysis_type == "visibility":
-        
-        visibility_voxel = np.full((nX,nY,nZ),0)
+
+        visibility_voxel = np.full((model3d.shape[0],model3d.shape[1],model3d.shape[2]),0)
         for z in range(model3d.shape[2]):
             visibility_map = model3d[:,:,z]
             iso_map = Isovist(visibility_map)

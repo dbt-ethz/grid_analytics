6538762309cb0eba13167e8bef6c12aeaa07fa3e
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2021-04-12 07:40:15 +0200

    add import of .distances

--- tools/__init__.py
+++ tools/__init__.py
@@ -4,6 +4,7 @@ from .shortest_path import *
 from .shadow import *
 from .utilities import *
 from .neighbors import *
+from .distances import *
 from .color_value import *
 
 __all__ = [name for name in dir() if not name.startswith('_')]

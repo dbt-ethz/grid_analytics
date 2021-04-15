31eb2ce6890f5508ac6e7407c8863776dbd15c2a
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2021-04-15 09:32:55 +0200

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,10 +2,8 @@ from .grid import *
 from .isovist import *
 from .shortest_path import *
 from .shadow import *
-from .utilities import *
 from .neighbors import *
 from .distances import *
-from .color_value import *
 from .shadow_Bresenham import *
 
 __all__ = [name for name in dir() if not name.startswith('_')]
